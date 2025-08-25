9f18ea665025859f6a2b0ca19b591e3d92e8d24d
Karina <karinaosoriof99@gmail.com>
2025-08-25 12:43:00 -0400

    feat: cambio el nombre de la tienda a Atheris

--- web/src/components/Carrito.tsx
+++ web/src/components/Carrito.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Producto } from '../interfaces/Producto';
 
 type Props = {}
 
 export const Carrito = (props: Props) => {
+  // Define el estado para almacenar los productos del carrito
+  const [productos, setProductos] = useState([]);
+
   return (
-    <div>Carrito</div>
+    <div>
+      <h2>Carrito</h2>
+      <p>Hay {productos.length} productos en el carrito.</p>
+    </div>
   )
 }
